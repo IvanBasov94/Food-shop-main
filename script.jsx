b4ce90db7fe41e761512ced21740b188e2281b4b
Ivan Basov <vanya.b94@mail.ru>
2023-01-20 13:51:37 +0300

    Commit 134

---
+++ script.jsx
@@ -0,0 +1,20 @@
+const hyman = {
+	firstName: 'Ivan',
+	lastName: 'Basov',
+	age: 28,
+	isProgrammer: true,
+};
+
+const helloWorld = () => {
+	console.log('Hello World');
+};
+
+const arr = [1, 1, 2, 3, 5, 8];
+
+class Animal {
+
+}
+
+const COLOR = '#fff';
+
+console.log(COLOR);
